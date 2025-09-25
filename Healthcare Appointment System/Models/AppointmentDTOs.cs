b075058c47e0e69f85db0e0f066df5188f4e2ff1
AyomideAlexanderBoye <boyeayomide@gmail.com>
2025-09-25 17:17:48 -0500

    done with patient and appointment DTOs

---
+++ Healthcare Appointment System/Models/AppointmentDTOs.cs
@@ -0,0 +1,23 @@
+﻿namespace Healthcare_Appointment_System.Models {
+    public class AppointmentDTO {
+        public int AppointmentId { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+        public string Slot { get; set; }
+        public string Note { get; set; }
+        public AppointmentReasons Reason { get; set; }
+        public Status AppointmentStatus { get; set; }
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+    }
+
+    public class CreateAppointmentDTO {
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public int ClinicId { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Duration { get; set; }
+        public AppointmentReasons Reason { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ Healthcare Appointment System/Models/PatientDTOs.cs
@@ -0,0 +1,23 @@
+﻿namespace Healthcare_Appointment_System.Models {
+    public class PatientDTO {
+        public int PatientId { get; set; }
+        public string FullName { get; set; }
+        public string PatientEmail { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public GenderList Gender { get; set; }
+    }
+
+    public class CreatePatientDTO {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PatientEmail { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public GenderList Gender { get; set; }
+        public string EmergencyContact { get; set; }
+    }
+
+    public class UpdatePatientDTO : CreatePatientDTO { }
+}
