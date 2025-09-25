f89c20b83aedfacd358d325a05df33aaa0007099
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-25 17:10:42 -0500

    Create GetDoctors HttpGet method

--- Healthcare Appointment System/Controllers/DoctorController.cs
+++ Healthcare Appointment System/Controllers/DoctorController.cs
@@ -1,7 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Healthcare_Appointment_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Healthcare_Appointment_System.Controllers {
-    public class DoctorController : Controller {
+    [Route("api/[controller")]
+    [ApiController]
+    public class DoctorController : ControllerBase {
+        private readonly HealthcareAppointmentSystemContext _context;
+
+        public DoctorController(HealthcareAppointmentSystemContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDoctors() {
+            List<Doctor> doctors = await _context.Doctors.ToListAsync();
+            return Ok(doctors);
+        }
+
+        //GET /api/doctors - Get all doctors (with specialty filtering)
+        // GET /api/doctors/{id}- Get doctor by ID
+        // POST /api/doctors - Add new doctor
+        // PUT /api/doctors/{id}-Update doctor information
+        //o	GET /api/doctors/{id}/ schedule - Get doctor's availability
+        //o	GET /api/doctors/specialties - Get all specialties
+
         public IActionResult Index() {
             return View();
         }
