b56db63569192cb23e51866c2a902f9c43d7e16f
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-26 12:14:01 -0500

    add migration

--- Healthcare Appointment System/Migrations/20250926171339_RenamePropertyInClinic.Designer.cs
+++ Healthcare Appointment System/Migrations/20250926171339_RenamePropertyInClinic.Designer.cs
@@ -4,6 +4,7 @@ using Healthcare_Appointment_System.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Healthcare_Appointment_System.Migrations
 {
     [DbContext(typeof(HealthcareAppointmentSystemContext))]
-    partial class HealthcareAppointmentSystemContextModelSnapshot : ModelSnapshot
+    [Migration("20250926171339_RenamePropertyInClinic")]
+    partial class RenamePropertyInClinic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
