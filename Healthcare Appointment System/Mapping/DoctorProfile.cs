5332832f26f6043f8527a1b34c7e376eddb7eeaa
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-25 22:11:57 -0500

    Create all mappings for every DTO

---
+++ Healthcare Appointment System/Mapping/DoctorProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Healthcare_Appointment_System.Models;
+
+namespace Healthcare_Appointment_System.Mapping {
+    public class DoctorProfile : Profile {
+        public DoctorProfile() {
+            CreateMap<Doctor, DoctorDTO>();
+            CreateMap<Appointment, AppointmentDTO>();
+            CreateMap<CreateDoctorDTO, Doctor>();
+            CreateMap<UpdateDoctorDTO, Doctor>();
+        }
+    }
+}
