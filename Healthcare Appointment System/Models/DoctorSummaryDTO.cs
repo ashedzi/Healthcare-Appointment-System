5603a484c9903adaafa0f2744c4da345cd9b0c38
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-25 20:43:57 -0500

    Add createdoctordto and updatedoctordto to doctorsummary dto

--- Healthcare Appointment System/Models/DoctorSummaryDTO.cs
+++ Healthcare Appointment System/Models/DoctorSummaryDTO.cs
@@ -9,4 +9,19 @@
         public TimeSpan AvailableEnd { get; set; }
         public int AppointmentDurationMinutes { get; set; }
     }
+
+    public class CreateDoctorDTO {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Specialty { get; set; }
+        public TimeSpan AvailableStart { get; set; }
+        public TimeSpan AvailableEnd { get; set; }
+        public int AppointmentDurationMinutes { get; set; }
+    }
+
+    public class UpdateDoctorDTO : CreateDoctorDTO {
+
+    }
 }
