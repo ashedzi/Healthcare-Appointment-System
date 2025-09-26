28daae1e1a26d4fc861bc4be6fd23bcba2971bdd
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-25 22:25:11 -0500

    create clinic DTO, CreateClinicDTO and UpdateClinicDTO classes

---
+++ Healthcare Appointment System/Models/ClinicDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Healthcare_Appointment_System.Models {
+    public class ClinicDTO {
+        public int ClinicId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class CreateClinicDTO {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class UpdateClinicDTO : CreateClinicDTO {
+
+    }
+}
