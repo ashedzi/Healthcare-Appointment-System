e9f9b40c851b1e99ce60a041e7e1c7c64bbef44b
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-25 22:20:34 -0500

    Set up controller

--- Healthcare Appointment System/Controllers/ClinicController.cs
+++ Healthcare Appointment System/Controllers/ClinicController.cs
@@ -1,7 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Healthcare_Appointment_System.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Healthcare_Appointment_System.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ClinicController : Controller {
+        private readonly HealthcareAppointmentSystemContext _context;
+        private readonly IMapper _mapper;
+
+        public ClinicController(HealthcareAppointmentSystemContext context, IMapper mapper) {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        //o	GET /api/clinics - Get all clinics
+
+        [HttpGet]
+        //public async Task<IActionResult> GetClinics()
+        //    o GET /api/clinics/{id
+        //} - Get clinic details with doctors
+        //    o   POST /api/clinics - Add new clinic
+
         public IActionResult Index() {
             return View();
         }
