1bcb0ba635a10fa535438ef228425ec09388c5e4
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-26 19:16:23 -0500

    Add new migration

---
+++ Healthcare Appointment System/Migrations/20250927000928_AddValidationAnnotations.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Healthcare_Appointment_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValidationAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Patients",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Patients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClinicAddress",
+                table: "Clinics",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Appointments",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "PatientId",
+                keyValue: 1,
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "PatientId",
+                keyValue: 2,
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "PatientId",
+                keyValue: 3,
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "PatientId",
+                keyValue: 4,
+                column: "IsDeleted",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "PatientId",
+                keyValue: 5,
+                column: "IsDeleted",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Patients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClinicAddress",
+                table: "Clinics",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
