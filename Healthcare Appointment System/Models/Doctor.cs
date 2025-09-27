3b15e057c9385a2e9d5e50cc32b58c32ba3476e0
Ashedzi Solomon <solomonashe84@gmail.com>
2025-09-26 19:05:10 -0500

    Add data annotations to the doctor model

--- Healthcare Appointment System/Models/Doctor.cs
+++ Healthcare Appointment System/Models/Doctor.cs
@@ -1,4 +1,6 @@
-﻿namespace Healthcare_Appointment_System.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Healthcare_Appointment_System.Models {
     public enum Specialty {
         GeneralPractitioner,  
         Pediatrician,   
@@ -13,17 +15,41 @@
     }
     public class Doctor {
         public int DoctorId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
         public string FullName {
             get { return $"{FirstName} {LastName}"; }
         }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
         public string LicenseNumber { get; set; }
+
+        [Required]
         public Specialty DoctorSpecialty { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage = "Invalid phone number.")]
         public string PhoneNumber { get; set; }
+
+        [Required]
         public TimeSpan AvailableStart { get; set; } = new TimeSpan(9, 0, 0);
-        public TimeSpan AvailableEnd { get; set; } = new TimeSpan(17, 0, 0);  
+
+        [Required]
+        public TimeSpan AvailableEnd { get; set; } = new TimeSpan(17, 0, 0);
+
+        [Required]
+        [Range(5, 480)]
         public int AppointmentDurationMinutes { get; set; } = 30;
         public ICollection<DoctorClinic> DoctorClinics { get; set; } = new List<DoctorClinic>();
         public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
